baff3e65be372506eb85a08b71244b1d4cc4a63c
KatoShunsuke <suke.shun.kato2@gmail.com>
2023-04-11 15:04:25 +0900

    deploy自動化: ログ出力でパーミッションエラーが出ていたのを修正

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -3,12 +3,14 @@
 sudo chown -R ec2-user:nginx /var/www
 # /var/www 以下全てのディレクトリのパーミッションを2775に変更する
 sudo find /var/www -type d -exec chmod 2775 {} \;
+# /var/www 以下全てのファイルのパーミッションを664に変更する
+sudo find /var/www -type f -exec chmod 664 {} \;
 
 cd /var/www
 composer install
 
 # envファイルの設定、初回だけ
-if [ -f .env ]; then
-  php artisan migrate
-fi
+#if [ -f .env ]; then
+#  php artisan migrate
+#fi
 
