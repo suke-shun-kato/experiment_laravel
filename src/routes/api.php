aef8d40d328d0ca5fe393333772a11fa040eceab
KatoShunsuke <suke.shun.kato2@gmail.com>
2023-04-12 15:04:33 +0900

    認証実装: Recipeもログイン認証しないとエラーになるようにした

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,11 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/users', [UserController::class, 'register']);
 Route::post('/users/login', [UserController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'me']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'me']);
 
+    Route::get('/recipes', [RecipeController::class, 'index']);
+    Route::get('/recipes/{id}', [RecipeController::class, 'show']);
+    Route::post('/recipes', [RecipeController::class, 'store']);
+    Route::patch('/recipes/{id}', [RecipeController::class, 'update']);
+    Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
+});
 
-Route::get('/recipes', [RecipeController::class, 'index']);
-Route::get('/recipes/{id}', [RecipeController::class, 'show']);
-Route::post('/recipes', [RecipeController::class, 'store']);
-Route::patch('/recipes/{id}', [RecipeController::class, 'update']);
-Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
