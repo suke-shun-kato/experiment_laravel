9883e3f87317f1a3980bfef67d1b15a715234f0c
KatoShunsuke <suke.shun.kato2@gmail.com>
2023-04-16 18:59:24 +0900

    画像アップロード: SQLログをlaravel.logに出力するようにした

--- src/config/app.php
+++ src/config/app.php
@@ -195,6 +195,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * カスタムプロバイダー
+         */
+        App\Providers\DatabaseQueryServiceProvider::class
     ],
 
     /*
